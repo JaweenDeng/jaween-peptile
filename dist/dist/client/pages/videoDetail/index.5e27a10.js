090b063ff9ceaee7ec55771c40bf297f6ea519d7
jianwen <1032782502@qq.com>
2021-09-30 10:50:07 +0800

    删除文件

--- dist/dist/client/pages/videoDetail/index.5e27a10.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{961:function(n,t,e){"use strict";e.r(t);var o={layout:"notab",components:{VideoDetailMain:e(610).a},head:function(){return{title:"视频播放—艾思云课堂"}}},l=e(22),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("VideoDetailMain")}),[],!1,null,null,null);t.default=component.exports}}]);
--- dist/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['authMiddle'] = require('..\\middleware\\authMiddle.js')
-middleware['authMiddle'] = middleware['authMiddle'].default || middleware['authMiddle']
-
-export default middleware
